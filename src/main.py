d57dc175e501719b5e8cd015b861f42d2cc8de4c
jpatel <p.jagrut@gmail.com>
2021-03-21 23:33:08 -0400

    oneline commit

--- src/main.py
+++ src/main.py
@@ -4,3 +4,4 @@ added main file
 added requirements.txt 
 git add . ==> will add changes only in the current directory only
 git add -A ==> will add changes irrespective of your current directory and will add all the changes
+git log --oneline ==> will give you condensed information on your commit
