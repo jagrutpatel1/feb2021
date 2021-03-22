9b0f324ba4654d4faf993f9a0cd0e4dbe8a98ea7
jpatel <p.jagrut@gmail.com>
2021-03-22 09:19:03 -0400

    feature 1 in rel_2.0

--- src/main.py
+++ src/main.py
@@ -5,3 +5,4 @@ added requirements.txt
 git add . ==> will add changes only in the current directory only
 git add -A ==> will add changes irrespective of your current directory and will add all the changes
 git log --oneline ==> will give you condensed information on your commit
+branching experiments
