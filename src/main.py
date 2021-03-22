b6828e0c3437af0cd3a40db119e741c1c0a3ed96
jpatel <p.jagrut@gmail.com>
2021-03-21 23:13:50 -0400

    read main.py

--- src/main.py
+++ src/main.py
@@ -2,3 +2,5 @@ This is developer podium
 omp
 added main file
 added requirements.txt 
+git add . ==> will add changes only in the current directory only
+git add -A ==> will add changes irrespective of your current directory and will add all the changes
